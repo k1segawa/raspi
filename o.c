565d31a91d56154886963309d29457ee1606f21b
k1segawa <k1segawa.july@gmail.com>
2015-09-15 11:19:14 +0900

    o.c v.2 (add argc,argv,define)

--- o.c
+++ o.c
@@ -3,6 +3,14 @@
 #include <stdlib.h>
 #include "wiringPi.h"
 
+#define DEBUG
+
+#ifdef DEBUG
+#define DPRINT	printf
+#else
+#define DPRINT
+#endif
+
 #define INPUT_PIN	(24)
 /* 24(BCM2835 Lib Pin No.) */
 /* 5(wiringPi Lib Command Pin No.) */
@@ -16,23 +24,39 @@
 #define S_BUF	(512)
 #define LIMIT	(40000)
 
-int main()
+int main(int argc, char *argv[])
 {
 	int rtn;
 	int v;
+
+/* OUTPUT */	
 #if 1
 	FILE *fp;
-	unsigned char b[S_BUF];
+	unsigned char b[S_BUF+1];
+	int o_pin_no;
+	int limit;
 
-	memset(b,0,S_BUF);
+	if( argc == 1 ) {
+		/* default */
+		o_pin_no = OUTPUT_PIN;
+		limit = LIMIT;
+	} else if( argc == 3 ) {
+		o_pin_no = atoi(argv[1]);
+		limit = atoi(argv[2]);
+	} else {
+		fprintf(stderr, "Usage:%s\n",argv[0]);
+		fprintf(stderr, "      %s <port No.> <limit>\n",argv[0]);
+		return 1;
+	}
+
 	fp = popen("cat /sys/class/thermal/thermal_zone0/temp", "r");
-	if(fp == NULL) {
+	if( fp == NULL ) {
 		fprintf(stderr, "fopen(): Open Error\n");
 		return 1;
 	} else {
 		fgets(b, S_BUF, fp);
 		pclose(fp);
-		fprintf(stdout, "temp val=%s", b);
+		DPRINT("temp val=%s", b);
 	}
 #endif
 
@@ -43,29 +67,31 @@ int main()
 	}
 
 #if 0
+/* INPUT */
 	pullUpDnControl(INPUT_PIN, PUD_UP);	/* pullup */
 	pinMode(INPUT_PIN, INPUT);
 
 	v = 0;
 	v = digitalRead(INPUT_PIN);
-	fprintf(stdout, "%d", v);
+	fprintf(stdout, "%d", v);	/* print read value */
 #else
+/* OUTPUT */
 	pinMode(OUTPUT_PIN, OUTPUT);
 
-	fprintf(stdout, "int size=%d\n", sizeof(int));
+	DPRINT("int size=%d\n", sizeof(int));
 	if(strcmp(b,"") == 0) {
 		fprintf(stderr, "b: Null Error\n");
 		return 1;
 	} else {
 		v = atoi(b);
 	}
-	fprintf(stdout, "v=%d\n",v);
+	DPRINT("v=%d\n",v);
 	if(v < LIMIT) {
 		digitalWrite(OUTPUT_PIN, 0);
-		fprintf(stdout, "0 output.\n");
+		DPRINT("0 output.\n");
 	} else {
 		digitalWrite(OUTPUT_PIN, 1);
-		fprintf(stdout, "1 output.\n");
+		DPRINT("1 output.\n");
 	}
 #endif
 	
