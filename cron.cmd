c692d872bdfa3592f03e5085f94fae9b27034cff
k1segawa <k1segawa.july@gmail.com>
2015-10-28 10:12:05 +0900

    crontab tempFan2 changed. Signed-off-by: k1segawa <k1segawa.july@gmail.com>

--- cron.cmd
+++ cron.cmd
@@ -1,3 +1,10 @@
 crontab -e
 * * * * * /usr/bin/sudo /home/pi/tempFan --out=23 --cpu=47000 1> /dev/null 2>/dev/null
 * * * * * /usr/bin/sudo /home/pi/pwoff --input=24 --time=120 1> /dev/null 2>/dev/null
+
+
+
+tempFan2:
+#* * * * * /usr/bin/sudo /home/pi/raspi/tempFan --out=23 --cpu=48000 1> /dev/null 2>>/dev/null
+* * * * * /usr/bin/sudo /home/pi/raspi/tempFan2 --out=18 --cpu=48000 --power 97 1> /dev/null 2>>/dev/null
+
